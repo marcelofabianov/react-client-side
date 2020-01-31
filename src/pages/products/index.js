6086bbbb9958cbd99392b0706fa6bb5b5b724549
Marcelo Fabiano <marcelofabiano@hotmail.com.br>
2020-01-31 15:30:31 -0300

    link voltar

--- src/pages/products/index.js
+++ src/pages/products/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 import api from "../../services/api";
 import "./styles.css";
@@ -18,12 +19,17 @@ export default class Product extends Component {
     const { product } = this.state;
 
     return (
-      <div className="product-info">
-        <h1>{product.title}</h1>
-        <p>{product.description}</p>
-        <p>
-          URL: <a href={product.url}>{product.url}</a>
-        </p>
+      <div className="container">
+        <div className="product-info">
+          <h1>{product.title}</h1>
+          <p>{product.description}</p>
+          <p>
+            URL: <a href={product.url}>{product.url}</a>
+          </p>
+        </div>
+        <div className="actions">
+          <Link to="/">Voltar</Link>
+        </div>
       </div>
     );
   }
